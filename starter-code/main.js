2a24c22512dfff2501e522cf22488ea1d9d1c285
Zachary Totta <ztotta@gmail.com>
2016-09-17 11:29:31 -0400

    added game logic ex 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again!");
+}
