afcc0296b7e6b23f5f534d1b90eab57cdaae1aff
Zachary Totta <ztotta@gmail.com>
2016-09-21 14:59:16 -0700

    added JS cards to game apparently

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,26 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again!");
-}
+//if (cardOne === cardTwo) {
+//	alert("You found a match!");
+//} else {
+//	alert("Sorry, try again!");
+//}
+
+// find the board and set it to a variable
+// so that we can add cards to it
+var board = document.getElementById('game-board');
+
+// function that will create your board
+function createBoard() {
+  // loop through your cards array to create card elements for your board
+  for (var i=0; i<cards.length; i++) {
+
+    var cardElement = document.createElement('div');
+
+    cardElement.className = 'card';
+
+    board.appendChild(cardElement);
+  }
+
+}; 
